d161ecee8bc2148d26714adcddd2b4dba7e09ce9
Tobias Verhoog <tv@newspark.nl>
2020-03-14 16:43:43 +0100

    Initial commit

---
+++ src/test/java/nl/newspark/cucumber/Main.java
@@ -0,0 +1,17 @@
+package nl.newspark.cucumber;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.concurrent.TimeUnit;
+
+public class Main {
+    public static void main(String[] args) throws InterruptedException{
+        System.setProperty("webdriver.chrome.driver", "src/test/resources/chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        driver.get("https://www.google.com");
+        Thread.sleep(5000);
+        driver.quit();
+    }
+}
